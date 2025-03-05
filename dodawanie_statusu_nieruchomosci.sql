601a0483d42fb670a492a0fb84d5dcb770d3a652
Michał Ślęzak <65869511+michal-slezak-dev@users.noreply.github.com>
2025-03-05 20:35:01 +0100

    Delete dodawanie_statusu_nieruchomosci.sql

--- dodawanie_statusu_nieruchomosci.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO status_nieruchomosci (status_nieruchomosci)
-VALUES ('do remontu'),
-       ('do odświeżenia'),
-       ('stan deweloperski'),
-       ('stan surowy otwarty'),
-       ('stan surowy zamknięty'),
-       ('stan pod klucz'),
-       ('czysta'),
-       ('zarośnięta');
